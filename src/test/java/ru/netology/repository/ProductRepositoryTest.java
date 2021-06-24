1f06e965ff86c24c0b6da95b4d3fdea71c780ee8
Evgenii Semenov <eo.semenoff@gmail.com>
2021-06-24 19:19:09 +0300

    NotFoundException added

--- src/test/java/ru/netology/repository/ProductRepositoryTest.java
+++ src/test/java/ru/netology/repository/ProductRepositoryTest.java
@@ -1,21 +1,38 @@
 package ru.netology.repository;
 
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 import ru.netology.domain.Book;
 import ru.netology.domain.Product;
+import ru.netology.exception.NotFoundException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductRepositoryTest {
-  private ProductRepository repository = new ProductRepository();
-  private Book coreJava = new Book();
+    private ProductRepository repository = new ProductRepository();
+    private Book coreJava = new Book();
+    private Product item = new Product(4, "Product for remove", 500);
 
-  @Test
-  public void shouldSaveOneItem() {
-    repository.save(coreJava);
+    @Test
+    public void shouldRemoveByIdIfExist() {
+        repository.save(item);
+        repository.save(coreJava);
+        repository.removeById(4);
+        Product[] expected = new Product[]{coreJava};
+        Product[] actual = repository.findAll();
+        assertArrayEquals(actual, expected);
+    }
 
-    Product[] expected = new Product[]{coreJava};
-    Product[] actual = repository.findAll();
-    assertArrayEquals(expected, actual);
-  }
+    @Test
+    public void shouldShowNotFoundExceptionIfRemoveByIdNotExist() {
+        repository.save(item);
+        repository.save(coreJava);
+        repository.removeById(20);
+        assertThrows(NotFoundException.class, () -> {
+            repository.removeById(15);
+        });
+    }
 }
+
